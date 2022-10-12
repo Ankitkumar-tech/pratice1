667513b6479d5468bc5008444829d6fcc039e6f8
Ankit Kumar <ankit8287670719@gmail.com>
2022-10-12 21:42:02 +0530

    first commit

---
+++ firstday-2 (1)/problem4.js
@@ -0,0 +1,22 @@
+// Define a variable called name with your Name as the assigned value
+// Print the value stored in the variable name
+// Change the variable to store your father's name
+// Print the value stored in the variable name
+// Change the variable again to store your mother's name.
+// Print the value stored in the variable name
+
+
+let name  ="ankit" ;
+
+
+console.log(name);
+
+name  ="sanjeev kumar" ;
+
+
+console.log(name);
+
+name  ="beena sharma " ;
+
+
+console.log(name)
